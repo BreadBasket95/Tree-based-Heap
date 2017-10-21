d9ef561b2ab0216736b824202d0c07e69cbb1ec9
Marc Moore <mnm419@msstate.edu>
2017-10-21 13:43:00 -0500

    Laying the Groundwork for the Heap the header files and implementation files been created but no substantial work has started

---
+++ TreeNode.h
@@ -0,0 +1,27 @@
+// Marc Moore 10-17.
+
+#ifndef TREENODE_H
+#define TREENODE_H
+
+#include <string>
+using namespace std;
+
+class TreeNode{
+  public:
+    string data;
+    TreeNode *parent = NULL;
+    TreeNode *left = NULL;
+    TreeNode *right = Null;
+    TreeNode
+}
+/*
+Member descriptions:
+data: the data to be stored by the node
+parent: a pointer to the parent of the node
+left: a pointer to the left child node
+right: a pointer to the right child node
+Method descriptions:
+THeapNode(string): constructor that receives the data to be stored by the node and sets the
+THeapNode pointers to NULL
+*/
+#endif
